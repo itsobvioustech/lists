e8126e528e16f93bb25b795ecf8d1e39667d8b84
Jebu Ittiachen <jebu@jebu.net>
2023-05-25 11:46:02 +0530

    token refresh

--- coingecko_mcap.py
+++ coingecko_mcap.py
@@ -6,7 +6,8 @@ from common import ChainId, Address
 
 
 def get_coingecko_mcap() -> dict[str, dict]:
-    coins = [httpx.get('https://api.coingecko.com/api/v3/coins/markets', params={'vs_currency': 'usd', 'per_page': 250, 'page': page}).json() for page in range(1,5)]
+    coins = [httpx.get('https://api.coingecko.com/api/v3/coins/markets', params={'vs_currency': 'usd', 'per_page': 250, 'page': page}).json() for page in range(1,4)]
+    # print(coins)
     market_cap_data: dict[str, dict] = {}
     for coin in coins:
         for coin_data in coin:
